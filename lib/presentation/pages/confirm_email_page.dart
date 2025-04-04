f207c6a52791b03883ded92d8c38b2b4c20766e9
Marco Papula <marco@split-labs.com>
2025-04-05 00:02:14 +0200

    add confirm email page

---
+++ lib/presentation/pages/confirm_email_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:neroia_app/core/localizations/translations_extension.dart';
+import 'package:neroia_app/core/theme/colors.dart';
+import 'package:neroia_app/core/theme/textstyles.dart';
+import 'package:neroia_app/features/widgets/neroia_background_gradient.dart';
+import 'package:neroia_app/features/widgets/neroia_button.dart';
+
+class ConfirmEmailPage extends ConsumerWidget {
+  const ConfirmEmailPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return NeroiaBackgroundGradient(
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 8.0,
+              children: [
+                HugeIcon(icon: HugeIcons.strokeRoundedMail01, color: ref.colors.text, size: 50),
+                Text(context.i18n.auth.confirmEmail, style: ref.textStyle.title),
+              ],
+            ),
+          ),
+          bottomNavigationBar: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              spacing: 8.0,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                NeroiaButton.primary(
+                  expandWidth: true,
+                  text: context.i18n.auth.login,
+                  onTap: () async {
+                    final isValid = Form.of(context).validate();
+                    if (!isValid) return;
+                  },
+                ),
+                NeroiaButton.secondary(expandWidth: true, text: context.i18n.auth.newHere, onTap: () {}),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
