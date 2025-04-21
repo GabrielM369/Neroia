d392ea60c4f665ff5686f7e14c5d6a81480c4782
Marco Papula <marco@split-labs.com>
2025-04-21 13:42:22 +0200

    show list of events on event page

--- lib/presentation/pages/events_page.dart
+++ lib/presentation/pages/events_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:neroia_app/features/events/presentation/events_list.dart';
+import 'package:neroia_app/features/profile/presentation/profile_picture_button.dart';
 
 class EventsPage extends ConsumerWidget {
   const EventsPage({super.key});
@@ -7,8 +9,19 @@ class EventsPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      backgroundColor: Colors.blue, // ref.colors.background,
-      body: Container(child: Center(child: Text('Events Page', style: TextStyle(fontSize: 24, color: Colors.white)))),
+      body: SafeArea(
+        bottom: false,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              Row(children: [ProfilePictureButton()]),
+              Container(child: Center(child: Text('Events Page', style: TextStyle(fontSize: 24, color: Colors.white)))),
+              Expanded(child: EventsList()),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
