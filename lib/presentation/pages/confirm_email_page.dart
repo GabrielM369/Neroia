7afd1f55b3b8fe374776da6d6352cf12c6439f89
Marco Papula <marco@split-labs.com>
2025-04-21 13:42:10 +0200

    format confirm email page

--- lib/presentation/pages/confirm_email_page.dart
+++ lib/presentation/pages/confirm_email_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hugeicons/hugeicons.dart';
 import 'package:neroia_app/core/localizations/translations_extension.dart';
+import 'package:neroia_app/core/router/neroia_routes.dart';
 import 'package:neroia_app/core/theme/colors.dart';
 import 'package:neroia_app/core/theme/textstyles.dart';
 import 'package:neroia_app/features/widgets/neroia_background_gradient.dart';
@@ -16,16 +18,18 @@ class ConfirmEmailPage extends ConsumerWidget {
       child: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 8.0,
-              children: [
-                HugeIcon(icon: HugeIcons.strokeRoundedMail01, color: ref.colors.text, size: 50),
-                Text(context.i18n.auth.confirmEmail, style: ref.textStyle.title),
-              ],
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 8.0,
+                children: [
+                  HugeIcon(icon: HugeIcons.strokeRoundedMail01, color: ref.colors.text, size: 50),
+                  Text(context.i18n.auth.confirmEmail, style: ref.textStyle.title),
+                ],
+              ),
             ),
           ),
           bottomNavigationBar: Padding(
@@ -36,13 +40,10 @@ class ConfirmEmailPage extends ConsumerWidget {
               children: [
                 NeroiaButton.primary(
                   expandWidth: true,
-                  text: context.i18n.auth.login,
-                  onTap: () async {
-                    final isValid = Form.of(context).validate();
-                    if (!isValid) return;
-                  },
+                  text: context.i18n.form.next,
+                  onTap: () => EventsRoute().replace(context),
                 ),
-                NeroiaButton.secondary(expandWidth: true, text: context.i18n.auth.newHere, onTap: () {}),
+                NeroiaButton.secondary(expandWidth: true, text: context.i18n.form.back, onTap: () => context.pop()),
               ],
             ),
           ),
